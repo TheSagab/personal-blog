e3ddf1e5ea705baf5533f7a3369a40c98a11bd5f
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2022-07-10 22:17:45 +0700

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -27,10 +27,10 @@ const Ultra = ({ cache }: { cache: Cache }) => {
       <Helmet>
         <title>Ultra</title>
         <link rel="stylesheet" href="/style.css" />
-        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          charSet="utf-8"
         />
       </Helmet>
       <main>
